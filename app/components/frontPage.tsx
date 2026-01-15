4cd235b046c1402df73cefb7612e3b3c43a104c1
Eirik Rognø <eirik@rogno.no>
2026-01-15 10:28:05 +0100

    Handle card width better on large screen

--- app/components/frontPage.tsx
+++ app/components/frontPage.tsx
@@ -31,7 +31,7 @@ export function FrontPage({ session, matches }: { session: Session, matches: Mat
         <Link to="/register">Registrer kamp</Link>
       </Button>
       <h2 className="text-xl mb-2">Nylige kamper:</h2>
-      <div className="w-full p-4 flex flex-col gap-2">
+      <div className="w-full max-w-lg p-4 flex flex-col gap-2">
         {matches.map(({ winnerId, matchCreatedAt, matchId, eloDelta, players: [player1, player2] }) => {
           const timeString = new Intl.DateTimeFormat("no", {
             weekday: "short",
@@ -42,16 +42,16 @@ export function FrontPage({ session, matches }: { session: Session, matches: Mat
             minute: "2-digit",
           }).format(matchCreatedAt);
           const eloString = (id: string) => id === winnerId ? `+${eloDelta}` : `-${eloDelta}`;
-          return <Card className="bg-white" key={matchId}>
+          return <Card className="bg-white md:px-4" key={matchId}>
             <CardHeader>
               <CardTitle className="text-center">{timeString}</CardTitle>
             </CardHeader>
             <CardContent className="grid grid-cols-[1fr_32px_2px_32px_1fr]">
-              <MatchParticipant {...player1} winner={winnerId === player1.id} className="justify-self-start" />
+              <MatchParticipant {...player1} winner={winnerId === player1.id} className="justify-self-start md:justify-self-center" />
               <p className="text-lg self-center justify-self-start">{eloString(player1.id)}</p>
               <Separator orientation="vertical" />
               <p className="text-lg self-center justify-self-end">{eloString(player2.id)}</p>
-              <MatchParticipant {...player2} winner={winnerId === player2.id} className="justify-self-end" />
+              <MatchParticipant {...player2} winner={winnerId === player2.id} className="justify-self-end md:justify-self-center" />
             </CardContent>
           </Card>
         })}
